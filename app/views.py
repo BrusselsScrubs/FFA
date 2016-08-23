0b9f3e4ee22ab649199a9accfd7fce3b96f4a70a
gableh <gable.heng@gmail.com>
2016-08-22 21:55:39 -0700

    add database

--- app/views.py
+++ app/views.py
@@ -1,7 +1,15 @@
 from app import app
-
+from databases.models import db
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/create')
+def create():
+    return "MX stuff"
+
+@app.route('/update/<id>')
+def edit():
+    return "MX stuff"
